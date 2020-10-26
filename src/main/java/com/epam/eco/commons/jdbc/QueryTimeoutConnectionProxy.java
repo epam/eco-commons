9aa41e9d3231fadab8c22a118e209ba249311659
Uladzislau B <Pandasut.vb@gmail.com>
2020-10-26 10:28:15 +0300

    Add sql connection wrapper that set default timeout for all statements (#6)
    * Add sql connection wrapper that set default timeout for all statements
    
    * Fix naming and add mockito for mocks
    
    * Merge same tests
    
    * Add null check
    
    Co-authored-by: Uladzislau Belykh <Uladzislau_Belykh@epam.com>

---
+++ src/main/java/com/epam/eco/commons/jdbc/QueryTimeoutConnectionProxy.java
@@ -0,0 +1,63 @@
+/*
+ *Copyright 2020 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.epam.eco.commons.jdbc;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.sql.Connection;
+import java.sql.Statement;
+
+import org.apache.commons.lang3.Validate;
+
+/**
+ * @author Uladzislau_Belykh
+ */
+public abstract class QueryTimeoutConnectionProxy {
+
+    public static Connection create(Connection instance, int queryTimeoutSec) {
+        Validate.isTrue(queryTimeoutSec >= 0, "Query timeout is invalid: %d", queryTimeoutSec);
+        Validate.notNull(instance, "Instance can't be null");
+
+        return (Connection) Proxy.newProxyInstance(
+                QueryTimeoutConnectionProxy.class.getClassLoader(),
+                new Class[]{Connection.class},
+                new QueryTimeoutConnectionInvocationHandler(instance, queryTimeoutSec));
+    }
+
+    private static class QueryTimeoutConnectionInvocationHandler implements InvocationHandler {
+
+        private final Connection instance;
+
+        private final int queryTimeoutSec;
+        public QueryTimeoutConnectionInvocationHandler(Connection instance, int queryTimeoutSec) {
+            this.instance = instance;
+            this.queryTimeoutSec = queryTimeoutSec;
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            Object result = method.invoke(instance, args);
+            if (result instanceof Statement) {
+                ((Statement) result).setQueryTimeout(queryTimeoutSec);
+            }
+            return result;
+        }
+    }
+
+    private QueryTimeoutConnectionProxy() {
+    }
+}
---
+++ src/test/java/com/epam/eco/commons/jdbc/QueryTimeoutConnectionProxyTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2020 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.epam.eco.commons.jdbc;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.apache.commons.lang3.mutable.MutableObject;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Uladzislau_Belykh
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class QueryTimeoutConnectionProxyTest {
+
+    @Mock
+    private Connection testConnection;
+    @Mock
+    private Statement testStatement;
+
+    @Test
+    public void testCreateFailsOnNegativeTimeout() {
+        Assert.assertThrows("Query timeout is invalid: -1", IllegalArgumentException.class,
+                () -> QueryTimeoutConnectionProxy.create(testConnection, -1));
+    }
+
+    @Test
+    public void testCreateFailsOnNullInstance() {
+        Assert.assertThrows("Instance can't be null", NullPointerException.class,
+                () -> QueryTimeoutConnectionProxy.create(null, 1));
+    }
+
+    @Test
+    public void testTimeoutIsSet() throws SQLException {
+        MutableObject<Integer> timeout = new MutableObject<>();
+        when(testConnection.createStatement()).thenReturn(testStatement);
+        doAnswer(invocationOnMock -> {
+            timeout.setValue((Integer) invocationOnMock.getArguments()[0]);
+            return null;
+        }).when(testStatement).setQueryTimeout(anyInt());
+        when(testStatement.getQueryTimeout()).thenAnswer(invocationOnMock -> timeout.getValue());
+
+        Connection wrappedConnection = QueryTimeoutConnectionProxy.create(testConnection, 0);
+        Assert.assertEquals(0, wrappedConnection.createStatement().getQueryTimeout());
+    }
+}
